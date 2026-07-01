2a9c4ea77f83937e6d89bb6d61cf5adb058e11fd
Lucas Reinert da Silva <lucas_silva187@estudante.sesisenai.org.br>
2026-07-01 16:07:25 -0300

    Add Main class with Carro functionality

---
+++ Aula2/Atividade 6/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void  main(String []args) {
+        Carro carro = new Carro();
+
+        carro.acelerar(50);
+        carro.frear(20);
+        carro.exibirVelocidade();
+    }
+}
