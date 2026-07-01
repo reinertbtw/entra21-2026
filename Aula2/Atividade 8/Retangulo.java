b3cb384992d77361d912313d6489d5da43fa5484
Lucas Reinert da Silva <lucas_silva187@estudante.sesisenai.org.br>
2026-07-01 16:29:52 -0300

    Create Retangulo.java

---
+++ Aula2/Atividade 8/Retangulo.java
@@ -0,0 +1,27 @@
+public class Retangulo {
+    private double largura = 6;
+    private double altura = 8;
+
+    public double calcularArea() {
+        return this.altura * this.largura;
+    }
+
+    public double calcularPerimetro() {
+        return this.altura + this.largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Altura: "+ getAltura());
+        System.out.println("Largura: " + getLargura());
+        System.out.println("Área: " + calcularArea());
+        System.out.println("Perímetro: " + calcularPerimetro());
+    }
+}
