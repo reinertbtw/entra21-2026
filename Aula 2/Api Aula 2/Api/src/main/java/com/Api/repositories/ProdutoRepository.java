b90db81faf1713e03a15a98d64540876ce89a900
Lucas Reinert da Silva <lucas_silva187@estudante.sesisenai.org.br>
2026-08-19 16:13:54 -0300

    Delete Aula 2/Api Aula 2 directory

--- Aula 2/Api Aula 2/Api/src/main/java/com/Api/repositories/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Api.repositories;
-
-import com.Api.entities.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
