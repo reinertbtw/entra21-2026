8ff8d3ebdcddeada13976dbbeeec6a32d2b7c507
Lucas Reinert da Silva <reinertwrk@gmail.com>
2026-08-19 16:17:07 -0300

    Add files via upload

---
+++ Aula 2/Api/src/main/java/com/Api/entities/Produto.java
@@ -0,0 +1,49 @@
+package com.Api.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String descricao;
+    private double preco;
+    private int estoque;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+}
